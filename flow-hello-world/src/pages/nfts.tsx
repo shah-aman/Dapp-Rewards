d9896db78122cc68e76972b408fea2bad9989124
Anurag Phadnis <phadnis.anurag@gmail.com>
2023-03-01 00:33:20 +0530

    add sample nft to react app

---
+++ flow-hello-world/src/pages/nfts.tsx
@@ -0,0 +1,239 @@
+import Head from 'next/head'
+import "../flow/config";
+import { useState, useEffect } from "react";
+import * as fcl from "@onflow/fcl";
+import { get } from 'https';
+
+export default function Home() {
+
+  const [user, setUser] = useState({loggedIn: null})
+  const [name, setName] = useState('')
+  let nftIds: number[] = [];
+  let nftUrls: string[] = [];
+  useEffect(() => fcl.currentUser.subscribe(setUser), [])
+
+  // NEW
+  const initAccount = async () => {
+    const transactionId = await fcl.mutate({
+      cadence: `
+      import FanexNft from 0x3d1a73afefe2d7f8
+
+      // This transaction configures a user's account
+      // to use the NFT contract by creating a new empty collection,
+      // storing it in their account storage, and publishing a capability
+      transaction {
+          prepare(acct: AuthAccount) {
+      
+              // Create a new empty collection
+              let collection <- FanexNft.createEmptyCollection()
+      
+              // store the empty NFT Collection in account storage
+              acct.save<@FanexNft.NFTCollection>(<-collection, to: FanexNft.CollectionStoragePath)
+      
+              log("Collection created for account 2")
+      
+              // create a public capability for the Collection
+              acct.link<&{FanexNft.NFTReceiver}>(FanexNft.CollectionPublicPath, target: FanexNft.CollectionStoragePath)
+      
+              log("Capability created")
+          }
+      }
+      `,
+      payer: fcl.authz,
+      proposer: fcl.authz,
+      authorizations: [fcl.authz],
+      limit: 50
+    })
+    
+    const transaction = await fcl.tx(transactionId).onceSealed()
+    console.log(transaction)
+  }
+  const getNftIds = async () => {
+    console.log(user.addr);
+    const nfts = await fcl.query({
+      cadence: `
+      // Print All NFTs
+
+      import FanexNft from 0x3d1a73afefe2d7f8
+      
+      // Print the NFTs owned by accounts 0x01 and 0x02.
+      pub fun main(address: Address): [UInt64] {
+      
+          // Get both public account objects
+          let account1 = getAccount(address)
+      
+          // Find the public Receiver capability for their Collections
+          let acct1Capability = account1.getCapability(FanexNft.CollectionPublicPath)
+      
+          // borrow references from the capabilities
+          let receiver1Ref = acct1Capability.borrow<&{FanexNft.NFTReceiver}>()
+              ?? panic("Could not borrow account 1 receiver reference")
+      
+          // Print both collections as arrays of IDs
+          log("Account 1 NFTs")
+          log(receiver1Ref.getIDs())
+          return receiver1Ref.getIDs();
+      }
+      `,
+      args: (arg, t) => [arg(user.addr, t.Address)]
+    })
+    console.log(nfts);
+    nftIds = nfts;
+  }
+
+  const getNftUrl = async (id: number) => {
+    console.log(user.addr);
+    const nft = await fcl.query({
+      cadence: `
+      // Print All NFTs
+
+      import FanexNft from 0x3d1a73afefe2d7f8
+      
+      // Print the NFTs owned by accounts 0x01 and 0x02.
+      pub fun main(address: Address, id: UInt64): String? {
+      
+          // Get both public account objects
+          let account1 = getAccount(address)
+      
+          // Find the public Receiver capability for their Collections
+          let acct1Capability = account1.getCapability(FanexNft.CollectionPublicPath)
+      
+          // borrow references from the capabilities
+          let receiver1Ref = acct1Capability.borrow<&{FanexNft.NFTReceiver}>()
+              ?? panic("Could not borrow account 1 receiver reference")
+      
+          // Print both collections as arrays of IDs
+          log("Account 1 NFTs")
+          log(receiver1Ref.getNftUrl(id: id))
+          return receiver1Ref.getNftUrl(id: id)
+      }
+      `,
+      args: (arg, t) => [arg(user.addr, t.Address), arg(id, t.UInt64)]
+    }) as string
+    return nft;
+  }
+
+  const withdrawNft = async () => {
+    const transactionId = await fcl.mutate({
+      cadence: `
+      import FanxToken from 0x3d1a73afefe2d7f8
+
+      transaction(amount: UInt64) {
+          prepare(account: AuthAccount) {
+              let tempVault <- FanxToken.createNonEmptyVault(balance: amount);
+      
+              let vaultRef = account.borrow<&FanxToken.Vault>(from: /storage/FanxTokenVault)
+                  ?? panic("Could not borrow reference of owner\'s vault");
+              
+              vaultRef.deposit(from: <-tempVault);
+      
+              log("Tokens despoited in account")
+          }
+      }   
+      `,
+      args: (arg, t) => [arg(10, t.UInt64)],
+      payer: fcl.authz,
+      proposer: fcl.authz,
+      authorizations: [fcl.authz],
+      limit: 50
+    })
+    
+    const transaction = await fcl.tx(transactionId).onceSealed()
+    console.log(transaction)
+  }
+
+  const mintNft = async () => {
+    const transactionId = await fcl.mutate({
+      cadence: `
+      import FanexNft from 0x3d1a73afefe2d7f8
+      import FanxToken from 0x3d1a73afefe2d7f8
+      
+      // This transaction allows the Minter account to mint an NFT
+      // and deposit it into its collection.
+      
+      transaction(url: String, amount: UInt64) {
+      
+          // The reference to the collection that will be receiving the NFT
+          let receiverRef: &{FanexNft.NFTReceiver}
+          // let vaultRef: &{FanxToken.Receiver}
+      
+          prepare(acct: AuthAccount) {
+              // Get the owner's collection capability and borrow a reference
+              self.receiverRef = acct.getCapability<&{FanexNft.NFTReceiver}>(FanexNft.CollectionPublicPath)
+                  .borrow()
+                  ?? panic("Could not borrow receiver reference")
+      
+              // self.vaultRef = account.borrow<&FanxToken.Vault>(from: /storage/FanxTokenVault)
+              //     ?? panic("Could not borrow reference of owner\'s vault");
+              
+          }
+      
+          execute {
+              // Use the minter reference to mint an NFT, which deposits
+              // the NFT into the collection that is sent as a parameter.
+              let newNFT <- FanexNft.mintNFT(url: url)
+      
+              self.receiverRef.deposit(token: <-newNFT)
+              // let tempVault <- vaultRef.withdraw(amount: amount);
+              // destroy tempVault;
+      
+              log("NFT Minted and deposited to Account 1's Collection")
+          }
+      }
+      `,
+      args: (arg, t) => [arg("https://ibb.co/HX6y3Yc", t.String), arg(10, t.UInt64)],
+      payer: fcl.authz,
+      proposer: fcl.authz,
+      authorizations: [fcl.authz],
+      limit: 50
+    });
+    const transaction = await fcl.tx(transactionId).onceSealed()
+    console.log(transaction)
+  }
+
+  const displayNfts = async () => {
+    await getNftIds();
+    for (const id of nftIds) {
+        const url = await getNftUrl(id);
+        console.log(url);
+    }
+  }
+
+  const AuthedState = () => {
+    return (
+      <div>
+        <div>Address: {user?.addr ?? "No Address"}</div>
+        <div>Profile Name: {name ?? "--"}</div>
+        {/* <button onClick={sendQuery}>Send Query</button> */}
+        <button onClick={initAccount}>Init Account</button> {/* NEW */}
+        <button onClick={mintNft}>Deposit NFT</button> {/* NEW */}
+        <button onClick={displayNfts}>Get Nfts</button>
+        <button onClick={fcl.unauthenticate}>Log Out</button>
+      </div>
+    )
+  }
+
+  const UnauthenticatedState = () => {
+    return (
+      <div>
+        <button onClick={fcl.logIn}>Log In</button>
+        <button onClick={fcl.signUp}>Sign Up</button>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>FCL Quickstart with NextJS</title>
+        <meta name="description" content="My first web3 app on Flow!" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <h1>Flow NFTS</h1>
+      {user.loggedIn
+        ? <AuthedState />
+        : <UnauthenticatedState />
+      }
+    </div>
+  )
+}
